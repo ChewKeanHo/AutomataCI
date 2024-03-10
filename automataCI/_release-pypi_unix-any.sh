2c43f72a7d94721e1ced199242962c5223579576
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2024-03-10 11:21:34 +0800

    automataCI: finalized _release-pypi CI job
    Since we're about to release a new version, we can go ahead and
    finalize _release-pypi CI job. Hence, let's do this.
    
    This patch finalizes _release-pypi CI job in automataCI/
    directory.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

--- automataCI/_release-pypi_unix-any.sh
+++ automataCI/_release-pypi_unix-any.sh
@@ -12,9 +12,17 @@
 # the License.
 . "${LIBS_AUTOMATACI}/services/io/fs.sh"
 . "${LIBS_AUTOMATACI}/services/io/os.sh"
-. "${LIBS_AUTOMATACI}/services/io/strings.sh"
 . "${LIBS_AUTOMATACI}/services/i18n/translations.sh"
 . "${LIBS_AUTOMATACI}/services/compilers/python.sh"
+
+
+
+
+# initialize
+if [ "$PROJECT_PATH_ROOT" = "" ]; then
+        >&2 printf "[ ERROR ] - Please run me from automataCI/ci.sh.ps1 instead!\n"
+        return 1
+fi
 
 
 
