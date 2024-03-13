060e5729fe671795e739bc10d774e84c623a3b0f
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2024-03-13 16:07:23 +0800

    srcGO: refactored start CI job for the i18n and latest specs
    The start CI job inside srcGO/ directory is completely outdated and
    not complying to the latest i18n and specs. Hence, let's refactor it.
    
    This patch refactors start CI job for the i18n and latest specs in
    srcGO/ directory.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

--- srcGO/.ci/start_unix-any.sh
+++ srcGO/.ci/start_unix-any.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright 2023  (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy of
@@ -15,30 +15,22 @@
 
 
 # initialize
-if [ "$PROJECT_PATH_ROOT" == "" ]; then
-        >&2 printf "[ ERROR ] - Please run from ci.cmd instead!\n"
+if [ "$PROJECT_PATH_ROOT" = "" ]; then
+        >&2 printf "[ ERROR ] - Please run from automataCI/ci.sh.ps1 instead!\n"
         return 1
 fi
 
-. "${PROJECT_PATH_ROOT}/${PROJECT_PATH_AUTOMATA}/services/io/os.sh"
-. "${PROJECT_PATH_ROOT}/${PROJECT_PATH_AUTOMATA}/services/compilers/go.sh"
+. "${LIBS_AUTOMATACI}/services/i18n/translations.sh"
+. "${LIBS_AUTOMATACI}/services/compilers/go.sh"
 
 
 
 
-# safety checking control surfaces
-OS_Print_Status info "checking go availability...\n"
-GO_Is_Available
-if [ $? -ne 0 ]; then
-        OS_Print_Status error "missing go compiler.\n"
-        return 1
-fi
-
-
-OS_Print_Status info "activating localized environment...\n"
+# execute
+I18N_Activate_Environment
 GO_Activate_Local_Environment
 if [ $? -ne 0 ]; then
-        OS_Print_Status error "activation failed.\n"
+        I18N_Activate_Failed
         return 1
 fi
 
@@ -46,11 +38,7 @@ fi
 
 
 # execute
-OS_Print_Status info "\n"
-OS_Print_Status note "IMPORTANT NOTICE\n"
-OS_Print_Status note "please perform the following command at your terminal manually:\n"
-OS_Print_Status note "    $ . ${PROJECT_GO_LOCALIZED}\n"
-OS_Print_Status info "\n"
+I18N_Guide_Start_Source "$PROJECT_GO_LOCALIZED"
 
 
 
