3f5cc00c2064379952a5932c34642201d5d63d14
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2024-03-14 07:38:46 +0800

    srcGO: refactored _package-docker CI job for the i18n and latest specs
    The _package-docker CI job inside srcGO/ directory is completely
    outdated and not complying to the latest i18n and specs. Hence, let's
    refactor it.
    
    This patch refactors _package-docker CI job for the i18n and latest
    specs in srcGO/ directory.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

--- srcGO/.ci/_package-docker_windows-any.ps1
+++ srcGO/.ci/_package-docker_windows-any.ps1
@@ -1,9 +1,9 @@
-# Copyright 2023  (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy
 # of the License at:
-#               http://www.apache.org/licenses/LICENSE-2.0
+#                http://www.apache.org/licenses/LICENSE-2.0
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,12 +15,13 @@
 
 # initialize
 if (-not (Test-Path -Path $env:PROJECT_PATH_ROOT)) {
-	Write-Error "[ ERROR ] - Please run from ci.cmd instead!\n"
+	Write-Error "[ ERROR ] - Please run from automataCI\ci.sh.ps1 instead!`n"
 	exit 1
 }
 
-. "${env:PROJECT_PATH_ROOT}\${env:PROJECT_PATH_AUTOMATA}\services\io\os.ps1"
-. "${env:PROJECT_PATH_ROOT}\${env:PROJECT_PATH_AUTOMATA}\services\io\fs.ps1"
+. "${env:LIBS_AUTOMATACI}\services\io\fs.ps1"
+. "${env:LIBS_AUTOMATACI}\services\io\strings.ps1"
+. "${env:LIBS_AUTOMATACI}\services\i18n\translations.ps1"
 
 
 
@@ -50,25 +51,35 @@ function PACKAGE-Assemble-DOCKER-Content {
 		return 10 # not applicable
 	} elseif ($(FS-Is-Target-A-Homebrew "${_target}") -eq 0) {
 		return 10 # not applicable
+	} elseif ($(FS-Is-Target-A-Cargo "${_target}") -eq 0) {
+		return 10 # not applicable
+	} elseif ($(FS-Is-Target-A-MSI "${_target}") -eq 0) {
+		return 10 # not applicable
 	}
-
-	OS-Print-Status info "Running Go specific content assembling function..."
 
 
 	# assemble the package
-	$__process = FS-Copy-File "${_target}" "${_directory}\${env:PROJECT_SKU}"
-	if ($__process -ne 0) {
+	$___dest = "${_directory}\${env:PROJECT_SKU}"
+	$null = I18N-Assemble "${_target}" "${___dest}"
+	$___process = FS-Copy-File "${_target}" "${___dest}"
+	if ($___process -ne 0) {
+		$null = I18N-Assemble-Failed
 		return 1
 	}
 
-	$__process = FS-Touch-File "${_directory}\.blank"
-	if ($__process -ne 0) {
+	$___dest = "${_directory}\.blank"
+	$null = I18N-Assemble "${_target}" "${___dest}"
+	$___process = FS-Copy-File "${_target}" "${___dest}"
+	if ($___process -ne 0) {
+		$null = I18N-Assemble-Failed
 		return 1
 	}
 
 
 	# generate the Dockerfile
-	$__process = FS-Write-File "${_directory}\Dockerfile" @"
+	$___dest = "${_directory}\Dockerfile"
+	$null = I18N-Create "${___dest}"
+	$___process = FS-Write-File "${___dest}" @"
 # Defining baseline image
 FROM --platform=${_target_os}/${_target_arch} scratch
 LABEL org.opencontainers.image.title=`"${env:PROJECT_NAME}`"
@@ -78,20 +89,32 @@ LABEL org.opencontainers.image.version=`"${env:PROJECT_VERSION}`"
 LABEL org.opencontainers.image.revision=`"${env:PROJECT_CADENCE}`"
 LABEL org.opencontainers.image.licenses=`"${env:PROJECT_LICENSE}`"
 "@
+	if ($___process -ne 0) {
+		$null = I18N-Create-Failed
+		return 1
+	}
 
-	if (-not ([string]::IsNullOrEmpty(${env:PROJECT_CONTACT_WEBSITE}))) {
-		$__process = FS-Append-File "${_directory}\Dockerfile" @"
+	if ($(STRINGS-Is-Empty "${env:PROJECT_CONTACT_WEBSITE}") -ne 0) {
+		$___process = FS-Append-File "${___dest}" @"
 LABEL org.opencontainers.image.url=`"${env:PROJECT_CONTACT_WEBSITE}`"
 "@
+		if ($___process -ne 0) {
+			$null = I18N-Create-Failed
+			return 1
+		}
 	}
 
-	if (-not ([string]::IsNullOrEmpty(${env:PROJECT_SOURCE_URL}))) {
-		$__process = FS-Append-File "${_directory}\Dockerfile" @"
+	if ($(STRINGS-Is-Empty "${env:PROJECT_SOURCE_URL}") -ne 0) {
+		$___process = FS-Append-File "${___dest}" @"
 LABEL org.opencontainers.image.source=`"${env:PROJECT_SOURCE_URL}`"
 "@
+		if ($___process -ne 0) {
+			$null = I18N-Create-Failed
+			return 1
+		}
 	}
 
-	$__process = FS-Append-File "${_directory}\Dockerfile" @"
+	$___process = FS-Append-File "${___dest}" @"
 # Defining environment variables
 ENV ARCH ${_target_arch}
 ENV OS ${_target_os}
@@ -107,7 +130,8 @@ EXPOSE 80
 # Set entry point
 ENTRYPOINT ["/app/bin/${env:PROJECT_SKU}"]
 "@
-	if ($__process -ne 0) {
+	if ($___process -ne 0) {
+		$null = I18N-Create-Failed
 		return 1
 	}
 
