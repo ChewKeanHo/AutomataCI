cd9cf07d2fa6ce6b75d973e1b041069d27571859
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2024-03-15 11:44:46 +0800

    srcNIM: refactored prepare CI job for i18n and latest specs
    The prepare CI job inside srcNIM/ directory is completely outdated
    and not complying to the latest i18n and specs. Hence, let's refactor
    it.
    
    This patch refactors prepare CI job for the i18n and latest specs in
    srcNIM/ directory.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

--- srcNIM/.ci/prepare_unix-any.sh
+++ srcNIM/.ci/prepare_unix-any.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright 2023  (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
+# Copyright 2023 (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy of
@@ -15,43 +15,32 @@
 
 
 # initialize
-if [ "$PROJECT_PATH_ROOT" == "" ]; then
-        >&2 printf "[ ERROR ] - Please run from ci.cmd instead!\n"
+if [ "$PROJECT_PATH_ROOT" = "" ]; then
+        >&2 printf "[ ERROR ] - Please run from automataCI/ci.sh.ps1 instead!\n"
         return 1
 fi
 
-. "${PROJECT_PATH_ROOT}/${PROJECT_PATH_AUTOMATA}/services/io/os.sh"
-. "${PROJECT_PATH_ROOT}/${PROJECT_PATH_AUTOMATA}/services/compilers/nim.sh"
-
-
-
-
-# safety checking control surfaces
-OS_Print_Status info "checking nim availability...\n"
-NIM_Is_Available
-if [ $? -ne 0 ]; then
-        OS_Print_Status error "missing nim compiler.\n"
-        return 1
-fi
-
-
-OS_Print_Status info "activating local environment...\n"
-NIM_Activate_Local_Environment
-if [ $? -ne 0 ]; then
-        OS_Print_Status error "activation failed.\n"
-        return 1
-fi
+. "${LIBS_AUTOMATACI}/services/i18n/translations.sh"
+. "${LIBS_AUTOMATACI}/services/compilers/nim.sh"
 
 
 
 
 # execute
-OS_Print_Status info "refreshing nimble repo list...\n"
+I18N_Activate_Environment
+NIM_Activate_Local_Environment
+if [ $? -ne 0 ]; then
+        I18N_Activate_Failed
+        return 1
+fi
+
+
+I18N_Import_Dependencies
 __current_path="$PWD" && cd "${PROJECT_PATH_ROOT}/${PROJECT_NIM}"
 nimble refresh
 if [ $? -ne 0 ]; then
         cd "$__current_path" && unset __current_path
-        OS_Print_Status error "refresh failed.\n"
+        I18N_Import_Failed
         return 1
 fi
 cd "$__current_path" && unset __current_path
