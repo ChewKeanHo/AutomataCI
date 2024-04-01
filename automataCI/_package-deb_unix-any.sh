2ea2a635a75b0573ee2025f6515cba98a50ed694
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2024-04-01 13:48:12 +0800

    automataCI: ensure deb packager reacts to 'any' os and arch properly
    The 'any' value for both target os and target arch are not properly
    handled for deb packager. This is a bug due to the misplaced
    response code handler. Hence, let's correct it.
    
    This patch ensures deb packager reacts to 'any' os and arch properly
    in automataCI/ directory.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

--- automataCI/_package-deb_unix-any.sh
+++ automataCI/_package-deb_unix-any.sh
@@ -56,11 +56,11 @@ PACKAGE_Run_DEB() {
         I18N_Check_Availability "DEB"
         DEB_Is_Available "$_target_os" "$_target_arch"
         case $? in
-        2|3)
+        2)
                 I18N_Check_Incompatible_Skipped
                 return 0
                 ;;
-        0)
+        0|3)
                 # accepted
                 ;;
         *)
--- automataCI/_package-deb_windows-any.ps1
+++ automataCI/_package-deb_windows-any.ps1
@@ -46,10 +46,10 @@ function PACKAGE-Run-DEB {
 	$null = I18N-Check-Availability "DEB"
 	$___process = DEB-Is-Available "${_target_os}" "${_target_arch}"
 	switch ($___process) {
-	{ $_ -in 2, 3 } {
+	2 {
 		$null = I18N-Check-Incompatible-Skipped
 		return 0
-	} 0 {
+	} { $_ -in 0, 3 } {
 		# accepted
 	} Default {
 		$null = I18N-Check-Failed
